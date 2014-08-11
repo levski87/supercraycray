07663bbf1248ddafb23013929d9b8bc6e53f702e
denis <denis>
2014-08-11 03:29:40 -0400

    page

--- wp-content/themes/warta-1.5.9/header.php
+++ wp-content/themes/warta-1.5.9/header.php
@@ -34,30 +34,6 @@ _taboola.push({article:'auto'});
 }(document.createElement('script'), document.getElementsByTagName('script')[0], 'http://cdn.taboola.com/libtrc/supercraycray/loader.js');
 </script>
 
-<script type='text/javascript'>
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
-(function() {
-var gads = document.createElement('script');
-gads.async = true;
-gads.type = 'text/javascript';
-var useSSL = 'https:' == document.location.protocol;
-gads.src = (useSSL ? 'https:' : 'http:') + 
-'//www.googletagservices.com/tag/js/gpt.js';
-var node = document.getElementsByTagName('script')[0];
-node.parentNode.insertBefore(gads, node);
-})();
-</script>
-
-<script type='text/javascript'>
-googletag.cmd.push(function() {
-googletag.defineSlot('/38082785/supercraycray_global_atf', [[728, 90], [970, 90]], 'div-gpt-ad-1407741736333-0').addService(googletag.pubads());
-googletag.defineSlot('/38082785/supercraycray_global_box', [[160, 600], [300, 250], [300, 600], [300, 1050], [336, 280]], 'div-gpt-ad-1407741736333-1').addService(googletag.pubads());
-googletag.pubads().enableSingleRequest();
-googletag.enableServices();
-});
-</script>
-
 <?php wp_head(); ?>
 </head>
 
@@ -171,11 +147,14 @@ googletag.enableServices();
                                 </div><!-- .navbar-collapse -->
                         </div><!--.container-->
                 </nav><!--#main-nav-->
-                    <div id="adunit-leader">
-                  <!-- supercraycray_global_atf -->
-                        <div id='div-gpt-ad-1407741218108-0'>
-                        <script type='text/javascript'>
-                        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1407741218108-0'); });
+<div id="adunit-leader">
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- leaderboard -->
+                        <ins class="adsbygoogle"
+                                 style="display:inline-block;width:728px;height:90px"
+                                 data-ad-client="ca-pub-4528087481844577"
+                                 data-ad-slot="2271573049"></ins>
+                    <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
                     </script>
-                    </div>
                 </div>
