34ccf1a41e491c66dae853b1b64934d2a486b6d4
denis <denis>
2014-08-12 02:22:16 -0400

    test

--- wp-content/themes/warta-1.5.9/header.php
+++ wp-content/themes/warta-1.5.9/header.php
@@ -34,6 +34,30 @@ _taboola.push({article:'auto'});
 }(document.createElement('script'), document.getElementsByTagName('script')[0], 'http://cdn.taboola.com/libtrc/supercraycray/loader.js');
 </script>
 
+<script type='text/javascript'>
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+(function() {
+var gads = document.createElement('script');
+gads.async = true;
+gads.type = 'text/javascript';
+var useSSL = 'https:' == document.location.protocol;
+gads.src = (useSSL ? 'https:' : 'http:') + 
+'//www.googletagservices.com/tag/js/gpt.js';
+var node = document.getElementsByTagName('script')[0];
+node.parentNode.insertBefore(gads, node);
+})();
+</script>
+
+<script type='text/javascript'>
+googletag.cmd.push(function() {
+googletag.defineSlot('/38082785/supercraycray_global_atf', [728, 90], 'div-gpt-ad-1407824428136-0').addService(googletag.pubads());
+googletag.defineSlot('/38082785/supercraycray_global_box', [[300, 250], [300, 600], [336, 280]], 'div-gpt-ad-1407824428136-1').addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.enableServices();
+});
+</script>
+
 <?php wp_head(); ?>
 </head>
 
@@ -148,13 +172,10 @@ _taboola.push({article:'auto'});
                         </div><!--.container-->
                 </nav><!--#main-nav-->
 <div id="adunit-leader">
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- leaderboard -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-4528087481844577"
-     data-ad-slot="2271573049"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
+<!-- supercraycray_global_atf -->
+<div id='div-gpt-ad-1407824428136-0' style='width:728px; height:90px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1407824428136-0'); });
 </script>
+</div>
                 </div>
