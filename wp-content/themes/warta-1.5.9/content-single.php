586e5cdcae9d681d182f79d9c6f2c808afbec235
denis <denis>
2014-08-19 00:03:28 -0400

    im a beast

--- wp-content/themes/warta-1.5.9/content-single.php
+++ wp-content/themes/warta-1.5.9/content-single.php
@@ -84,7 +84,8 @@ global $page, $pages;
 
 // This shows the Previous link
 wp_link_pages( array( 'before' => '<div class="page-links" style="display: inline-block !important;">', 
-'after' => '</div>', 'previouspagelink' => '← Previous', ); 
+'after' => '</div>', 'previouspagelink' => '← Previous', 
+'next_or_number' => 'next' ) ); 
 ?>
 
 <div class="page-count" style="display: inline-block !important;">
