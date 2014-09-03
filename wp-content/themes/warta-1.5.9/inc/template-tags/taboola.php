b4014657dcb5d57847818aa785c2731649237824
denis <denis>
2014-09-02 21:25:49 -0400

    good

--- wp-content/themes/warta-1.5.9/inc/template-tags/taboola.php
+++ wp-content/themes/warta-1.5.9/inc/template-tags/taboola.php
@@ -12,14 +12,28 @@ if( !function_exists('taboola') ) :
 function taboola() {
 ?> 
 
-<div class="taboola-padding">
-    <div id='taboola-below-main-column-mix'>
-        <script type="text/javascript">
-            window._taboola = window._taboola || [];
-            _taboola.push({mode:'thumbs-2r', container:'taboola-below-main-column-mix', placement:'below-main-column', target_type:'mix'});
-        </script>
-    </div>
-</div>
+<div id="contentad26326" style="text-align: center;"></div>
+<script type="text/javascript">
+    (function() {
+        var params =
+        {
+            id: "f44ac2e4-293e-4b3f-a6d8-f4f19f7218bc",
+            d:  "c3VwZXJjcmF5Y3JheS5jb20=",
+            wid: "26326",
+            cb: (new Date()).getTime()
+        };
+
+        var qs="";
+        for(var key in params){qs+=key+"="+params[key]+"&"}
+        qs=qs.substring(0,qs.length-1);
+        var s = document.createElement("script");
+        s.type= 'text/javascript';
+        s.src = "http://api.content.ad/Scripts/widget.aspx?" + qs;
+        s.async = true;
+        document.getElementById("contentad26326").appendChild(s);
+    })();
+</script>
+
     <?php
 }
 endif; // warta_page_title
