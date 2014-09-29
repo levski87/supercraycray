184e96dcd473bf44be7f9c57e576e7a9353af2da
kirill <kirill>
2014-09-29 01:21:46 -0400

    moved ads

--- wp-content/themes/warta-1.5.9/content-single.php
+++ wp-content/themes/warta-1.5.9/content-single.php
@@ -17,22 +17,7 @@ $userAgent = new Mobile_Detect();
 global $page, $pages, $numpages;
 ?>
 
-<?php if ($userAgent->isMobile() && ($page < $numpages)) : ?>
-
-    <div class="adunit">
-        <div style="font-size: 10px;">Advertisement</div>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- 300x250 Mobile Ad -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:300px;height:250px"
-             data-ad-client="ca-pub-4528087481844577"
-             data-ad-slot="9636387047"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
-
-<?php elseif ($page < $numpages) :?>
+<?php if (!$userAgent->isMobile() && $page < $numpages) :?>
     <div class="adunit">
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <!-- leaderboard -->
@@ -107,14 +92,15 @@ global $page, $pages, $numpages;
 </article>
 
 <?php if ($userAgent->isMobile() && ($page < $numpages)) : ?>
+
     <div style="text-align:center; padding-bottom:8px;">
         <div style="font-size: 10px;">Advertisement</div>
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- 300x100 Mobile Ad -->
+        <!-- 300x250 Mobile Ad -->
         <ins class="adsbygoogle"
-             style="display:inline-block;width:320px;height:100px"
+             style="display:inline-block;width:300px;height:250px"
              data-ad-client="ca-pub-4528087481844577"
-             data-ad-slot="5745754243"></ins>
+             data-ad-slot="9636387047"></ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
