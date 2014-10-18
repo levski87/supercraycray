c47610885db99c2b751aa27c9bc059f2a5829e39
Simon Yousoufov <simon.yous@gmail.com>
2014-10-18 16:45:25 -0400

    Mobile fixes, workaround for image cropping. Still need to clean up code

--- wp-content/themes/supercraycray/content-archive.php
+++ wp-content/themes/supercraycray/content-archive.php
@@ -58,7 +58,7 @@ global $friskamax_warta, $friskamax_warta_var;
 <script>
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
