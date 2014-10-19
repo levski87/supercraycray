623d79a91d2bff616523ae4f69f26a987d9ccda9
Simon Yousoufov <simon.yous@gmail.com>
2014-10-19 00:17:16 -0400

    Changed some stuff up for the better

--- wp-content/themes/supercraycray/content-archive.php
+++ wp-content/themes/supercraycray/content-archive.php
@@ -17,23 +17,23 @@ global $friskamax_warta, $friskamax_warta_var;
 <?php                                   $friskamax_warta_var['sidebar_counter'] = 0;
                                         dynamic_sidebar('archive-before-content-section');  ?>                    
                                 </div>
-<?php 
-                                /**
-                                 * Gets posts in rows of 3
-                                 */
-                                if ( have_posts() ) : 
-                                        while ( have_posts() ) : ?>
-                                                <div class="row">
-                                                        <?php for ($i=0; $i<3; $i++):?>
-                                                                 <?php the_post();?>
-                                                
-                                                                <?php get_template_part( 'content', isset($friskamax_warta['archive_layout']) ? $friskamax_warta['archive_layout'] : 1 );?>
-                                                        <?php endfor; ?>
+                                        <div class="row" id='inf-content-scroll'>
+<?php   
+                                        /**
+                                         * Gets posts in rows of 3
+                                         */
+                                        if ( have_posts() ) : 
+                                                while ( have_posts() ) : ?>
+                                                                <?php for ($i=0; $i<3; $i++):?>
+                                                                         <?php the_post();?>
+                                                        
+                                                                        <?php get_template_part( 'content', isset($friskamax_warta['archive_layout']) ? $friskamax_warta['archive_layout'] : 1 );?>
+                                                                <?php endfor; ?>
+                                                <?php endwhile; ?>
                                                 </div>
-                                        <?php endwhile; 
-                                else : 
-                                        get_template_part( 'content', 'none' ); 
-                                endif; 
+<?php                                   else : 
+                                                get_template_part( 'content', 'none' ); 
+                                        endif; 
 ?>
                                 <div class="row aside">
 <?php                                   $friskamax_warta_var['sidebar_counter'] = 0;
