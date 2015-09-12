1ba2115ff2f1e65d1f23889384ff1612b8fcb76e
denis <denisganev@gmail.com>
2015-09-12 17:20:15 -0400

    all

---
+++ wp-content/themes/warta-1.5.9/partials/ads-dfp-btf.php
@@ -0,0 +1,11 @@
+<!-- /38082785/supercracray_box_btf -->
+ <div style="text-align: center;">
+        <div style="display: inline-block;">
+      <div style="font-size: 10px;">Advertisement</div>
+<div id='div-gpt-ad-1442092146575-1'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1442092146575-1'); });
+</script>
+</div>
+</div>
+</div>
