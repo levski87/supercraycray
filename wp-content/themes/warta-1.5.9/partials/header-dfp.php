bc497811380eb86a7f7fb185e34c9e58b5cb5faf
denis <denisganev@gmail.com>
2015-09-16 21:14:13 -0400

    all

--- wp-content/themes/warta-1.5.9/partials/header-dfp.php
+++ wp-content/themes/warta-1.5.9/partials/header-dfp.php
@@ -21,6 +21,7 @@ var mappingTop = googletag.sizeMapping().addSize([765, 300], [[728, 90]]).addSiz
     googletag.defineSlot('/38082785/supercraycray_atf_leaderboard', [[320, 100], [300, 100], [300, 250], [728, 90], [320, 50]], 'div-gpt-ad-1442092146575-0').defineSizeMapping(mappingTop).addService(googletag.pubads());
     googletag.defineSlot('/38082785/supercracray_box_btf', [[300, 250], [336, 280]], 'div-gpt-ad-1442092146575-1').addService(googletag.pubads());
     googletag.defineSlot('/38082785/supercraycray_box_rightrail', [[160, 600], [300, 600], [300, 1050]], 'div-gpt-ad-1442092146575-2').addService(googletag.pubads());
+    googletag.defineOutOfPageSlot('/38082785/SuperCrayCray_OOP', 'div-gpt-ad-1442452357030-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.enableServices();
