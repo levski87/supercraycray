936d7f162644f2ea3b44a1622a80cde977260c06
denis <denisganev@gmail.com>
2015-12-15 22:19:40 -0500

    all

---
+++ wp-content/themes/warta-1.5.9/partials/mobile-ads-dfp-btf.php
@@ -0,0 +1,12 @@
+<?php if ($userAgent->isMobile()) : ?>
+	 <div style="text-align: center; padding-bottom:50px !important">
+        <div style="display: inline-block;">
+      <div style="font-size: 10px;">Advertisement</div>
+<div id='div-gpt-ad-1442092146575-1'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1442092146575-1'); });
+</script>
+</div>
+</div>
+</div>
+<?php endif; ?>
