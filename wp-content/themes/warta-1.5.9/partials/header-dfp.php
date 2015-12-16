b06eabe3995e4ed2203b31f8c1cc19486d87e12e
denis <denisganev@gmail.com>
2015-12-15 22:38:03 -0500

    all

--- wp-content/themes/warta-1.5.9/partials/header-dfp.php
+++ wp-content/themes/warta-1.5.9/partials/header-dfp.php
@@ -15,11 +15,11 @@
 </script>
 <script type='text/javascript'>
   googletag.cmd.push(function() {
-var mappingTop = googletag.sizeMapping().addSize([765, 300], [[728, 90]]).addSize([0, 0], [[320, 50], [300,100], [320,100], [300,250], [300, 50]]).build();
+var mappingTop = googletag.sizeMapping().addSize([765, 300], [[728, 90], [970, 90], [970,250]]).addSize([0, 0], [[320, 50], [300,100], [320,100], [300,250], [300, 50]]).build();
 
-    googletag.defineSlot('/38082785/supercraycray_atf_leaderboard', [[320, 100], [300, 100], [300, 250], [728, 90], [320, 50]], 'div-gpt-ad-1442092146575-0').defineSizeMapping(mappingTop).addService(googletag.pubads());
+    googletag.defineSlot('/38082785/supercraycray_atf_leaderboard', [[320, 100], [300, 100], [300, 250], [728, 90], [970, 90], [970, 250] [320, 50]], 'div-gpt-ad-1442092146575-0').defineSizeMapping(mappingTop).addService(googletag.pubads());
     googletag.defineSlot('/38082785/supercracray_box_btf', [[300, 250], [336, 280]], 'div-gpt-ad-1442092146575-1').addService(googletag.pubads());
-    googletag.defineSlot('/38082785/supercraycray_box_rightrail', [[160, 600], [300, 600], [300, 1050]], 'div-gpt-ad-1442092146575-2').addService(googletag.pubads());
+    googletag.defineSlot('/38082785/supercraycray_box_rightrail', [[160, 600], [300,250] ,[300, 600], [300, 1050]], 'div-gpt-ad-1442092146575-2').addService(googletag.pubads());
     googletag.defineOutOfPageSlot('/38082785/SuperCrayCray_OOP', 'div-gpt-ad-1442452357030-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
