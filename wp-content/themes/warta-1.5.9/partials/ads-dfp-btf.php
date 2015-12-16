efda357153843c8800832fa0010fbdc2bb4c4c7b
denis <denisganev@gmail.com>
2015-12-15 21:31:57 -0500

    all

--- wp-content/themes/warta-1.5.9/partials/ads-dfp-btf.php
+++ wp-content/themes/warta-1.5.9/partials/ads-dfp-btf.php
@@ -1,5 +1,5 @@
 <!-- /38082785/supercracray_box_btf -->
- <div style="text-align: center; padding-bottom:50px !important">
+ <div style="text-align: center; margin-bottom:75px !important">
         <div style="display: inline-block;">
       <div style="font-size: 10px;">Advertisement</div>
 <div id='div-gpt-ad-1442092146575-1'>
